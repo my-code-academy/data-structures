a21df4aa2e107f40e901f94ad419a83c7d28a080
Hemant <hhsriteja@gmail.com>
2019-01-26 00:00:58 +0530

    improved solution

--- Data-Structures/main.js
+++ Data-Structures/main.js
@@ -17,63 +17,85 @@ const rotateLeftFromPosition = (array, pos) => {
 
 const insertAtLeftPosition = (array, currElement, maxElementIndex) => {
     let currIndex = maxElementIndex;
-    while(array[currIndex] > currElement) {
-        array[currIndex+1] = array[currIndex];
+    while (array[currIndex] > currElement) {
+        array[currIndex + 1] = array[currIndex];
         currIndex--;
     }
-    array[currIndex+1] = currElement;
+    array[currIndex + 1] = currElement;
     return array;
 }
 
 const insertAtRightPosition = (array, currElement, minElementIndex) => {
     let currIndex = minElementIndex;
-    while(array[currIndex] < currElement) {
-        array[currIndex+1] = array[currIndex];
+    while (array[currIndex] < currElement) {
+        array[currIndex + 1] = array[currIndex];
         currIndex++;
     }
-    array[currIndex-1] = currElement;
+    array[currIndex - 1] = currElement;
     return array;
 }
 
-const testFunction = (inputArray) => {
-    inputArray = [25, 57, 37, 48, 12, 92, 86, 33];
-    // inputArray = [1,2,3,4,5];
-    if (hasNegativeElements(inputArray)) {
-        return new Error('invalid input: Input has negative elements');
-    } else if(hasDuplicates(inputArray)) {
-        return new Error('invalid input: Input has duplicate elements');
+const findPositionToInsert = (array, currElement) => {
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] < currElement && array[i + 1] > currElement && array[i] != -1) {
+            return i + 1;
+        }
+        if (array[i] > currElement && array[i + 1] < currElement && array[i] != -1) {
+            return i;
+        }
     }
-    let resultArray = [-1, -1, -1, -1, -1, -1, -1];
-
-    let minElementIndex = -1, maxElementIndex  = -1;
-
-    for(let i = 0; i < inputArray.length; i++) {
-        let currentNumber = inputArray[i];
-        if (minElementIndex === -1 && maxElementIndex === -1) {
-            resultArray[0] = currentNumber;
-            minElementIndex = -1;
-            maxElementIndex = 0;
-        } else if(currentNumber > resultArray[maxElementIndex] && minElementIndex === -1) {
-            resultArray[maxElementIndex+1] = currentNumber;
-            maxElementIndex++;
-        } else if(currentNumber < resultArray[0] && minElementIndex === -1) {
-            resultArray[resultArray.length-1] = currentNumber;
-            minElementIndex = inputArray.length-1;
-        } else if(currentNumber < resultArray[minElementIndex]) {
-            resultArray[minElementIndex-1] = currentNumber;
-            minElementIndex--;
-        } else if(currentNumber < resultArray[maxElementIndex] && minElementIndex === -1) {
-            resultArray = insertAtLeftPosition(resultArray, currentNumber, maxElementIndex);
-            maxElementIndex++;
-        } 
-    };
-    
-    console.log(resultArray);
 }
 
-testFunction([4,5]);
+inputArray = [25, 57, 37, 48, 12, 92, 86, 33];
+// inputArray = [1,2,3,4,5];
+if (hasNegativeElements(inputArray)) {
+    return new Error('invalid input: Input has negative elements');
+} else if (hasDuplicates(inputArray)) {
+    return new Error('invalid input: Input has duplicate elements');
+}
+let resultArray = [-1, -1, -1, -1, -1, -1, -1, -1];
 
-//console.log(insertAtRightPosition([12,14,-1,-1,6,7], 3, 4));
-//testFunction([2,3,4,-5,6]);
+let minElementIndex = -1, maxElementIndex = -1;
 
-module.exports = {testFunction};
+for (let i = 0; i < inputArray.length; i++) {
+    let currentNumber = inputArray[i];
+    if (minElementIndex === -1 && maxElementIndex === -1) {
+        resultArray[0] = currentNumber;
+        minElementIndex = -1;
+        maxElementIndex = 0;
+    } else if (currentNumber > resultArray[maxElementIndex]) {
+        resultArray[maxElementIndex + 1] = currentNumber;
+        maxElementIndex++;
+    } else if (currentNumber < resultArray[0] && minElementIndex === -1) {
+        resultArray[resultArray.length - 1] = currentNumber;
+        minElementIndex = inputArray.length - 1;
+    } else if (currentNumber < resultArray[minElementIndex]) {
+        resultArray[minElementIndex - 1] = currentNumber;
+        minElementIndex--;
+    } else {
+        let pos = findPositionToInsert(resultArray, currentNumber);
+        if (pos <= maxElementIndex) {
+            if (maxElementIndex - pos < inputArray.length - minElementIndex + pos) {
+                resultArray = insertAtLeftPosition(resultArray, currentNumber, maxElementIndex);
+                maxElementIndex++;
+            } else {
+                for (let j = 0; j < pos; j++) {
+                    let index = resultArray.indexOf(-1);
+                    if (index > -1) {
+                        resultArray.splice(index, 1);
+                    }
+                }
+                resultArray = rotateRightFromPosition(resultArray, pos);
+                resultArray.unshift(currentNumber);
+            }
+        }
+    }
+
+    // if(Math.min(Math.abs(maxElementIndex-pos), Math.abs(array.length-maxElementIndex-pos)) < Math.min(Math.abs(minElementIndex-pos), Math.abs(array.length-minElementIndex-pos))) {
+    //     resultArray = insertAtLeftPosition(resultArray, currentNumber, maxElementIndex);
+    //     maxElementIndex++;
+    // }
+};
+console.log(resultArray);
+
+//module.exports = { testFunction };
